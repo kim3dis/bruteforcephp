2ef0363b41806751cbd5da88cec3be86e7584333
kim3dis <kim3dis@gmail.com>
2017-09-12 19:03:43 -0300

    deixando mais bonitinho e adicionando animate.css, bootstrap e jquery(ui tbm)

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ $(document).ready(function(){
   $(".LETRAS_on").hide();
   $(".number_on").hide();
   $(".caracter_on").hide();
+  $("#spinnerLimite").hide();
+  $("#lbl_spinnerLimite").hide();
+  $("#lbl_spinnerLimite2").hide();
+  $("#modo_turbo").hide();
+
+  $("#lbl_modo_turbo").hide();
 
     $(".showhide_more").click(function(){
         $("#letras_on").toggle();
@@ -17,5 +23,11 @@ $(document).ready(function(){
         $(".LETRAS_on").toggle();
         $(".number_on").toggle();
         $(".caracter_on").toggle();
+        $("#spinnerLimite").toggle();
+        $("#lbl_spinnerLimite").toggle();
+        $("#lbl_spinnerLimite2").toggle();
+        $("#modo_turbo").toggle();
+        $("#lbl_modo_turbo").toggle();
+
     });
 });
--- sys.php
+++ sys.php
@@ -1,7 +1,32 @@
-<?php #sistema
-echo "<div style='overflow:scroll; width:800px; height:600;'>";
 
-$senha = $_POST["senha"];
+<script src="cronometro.js"></script>
+<link href="custom_cronometro.css" rel="stylesheet">
+<script>
+  iniciar();
+</script>
+
+<div class='quadro' style='width:1000px; height600px;'>
+  <div class='quadro' style='border: solid 0px #000; width:450px; height: 100px; float: right;'>
+    <center>
+      <p id='counter'>00:00:00</p><br>
+    </center>
+  </div>
+
+  <div style='overflow:scroll; width:500px; height:600px; float:left;'>
+
+
+
+<?php #sistema
+$senha = $_POST["pass"];
+
+
+if ($senha == "") {
+  header("Location:index.php");
+}
+
+if (isset($_POST['letras_on']) || isset($_POST['LETRAS_on']) || isset($_POST['number_on']) || isset($_POST['caracter_on'])) {
+
+
 
 
 if (isset($_POST['letras_on'])) {
@@ -427,7 +452,49 @@ if (isset($_POST['caracter_on']) && isset($_POST['number_on']) && isset($_POST['
 }
 // VARIAÇÕES COM MAIUSCULAS
 
-
+if (isset($_POST['letras_on']) && isset($_POST['number_on']) && isset($_POST['LETRAS_on'])) {
+  $qntd = 62;
+  $primeiro = array("a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w",
+  "x", "y", "z", "A", "B", "C", "D", "E",
+  "F", "G", "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+  "X", "Y", "Z", "0", "1", "2", "3", "4",
+"5", "6", "7", "8", "9");
+  $segundo = array("a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w",
+  "x", "y", "z", "A", "B", "C", "D", "E",
+  "F", "G", "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+  "X", "Y", "Z", "0", "1", "2", "3", "4",
+"5", "6", "7", "8", "9");
+  $terceiro = array("a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w",
+  "x", "y", "z", "A", "B", "C", "D", "E",
+  "F", "G", "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+  "X", "Y", "Z", "0", "1", "2", "3", "4",
+"5", "6", "7", "8", "9");
+  $quarto = array("a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w",
+  "x", "y", "z", "A", "B", "C", "D", "E",
+  "F", "G", "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+  "X", "Y", "Z", "0", "1", "2", "3", "4",
+"5", "6", "7", "8", "9");
+  $quinto = array("a", "b", "c", "d", "e",
+  "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w",
+  "x", "y", "z", "A", "B", "C", "D", "E",
+  "F", "G", "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+  "X", "Y", "Z", "0", "1", "2", "3", "4",
+"5", "6", "7", "8", "9");
+}
 
 
 
@@ -442,137 +509,303 @@ $_5 = 0;
 
 $tentativas_feitas = 0;
 
-//um caractere
-while ($senha != $bruteforce) {
-  $bruteforce = (string) "$primeiro[$_1]";
-  echo "$bruteforce";
-  echo "<br />";
-  $tentativas_feitas++;
-  $_1++;
-  if ($_1 == $qntd) {
-    $_1 = 0;
-    break;
+
+
+if(isset($_POST['modo_turbo'])) {
+  $turbo = strlen($senha);
+
+  switch ($turbo) {
+    case 1:
+      while ($senha != $bruteforce) {
+        $bruteforce = (string) "$primeiro[$_1]";
+        echo "$bruteforce";
+        echo "<br />";
+        $tentativas_feitas++;
+        $_1++;
+        if ($_1 == $qntd) {
+          $_1 = 0;
+          break;
+        }
+      }
+    break; // parar o case 1;
+
+    case 2:
+      while ($senha != $bruteforce) {
+        $bruteforce = "$segundo[$_2]$primeiro[$_1]";
+        echo "$bruteforce";
+        echo "<br />";
+        $_1++;
+        $tentativas_feitas++;
+        if ($_1 == $qntd) {
+          $_1 = 0;
+          $_2++;
+        }
+        if ($_2 == $qntd) {
+          $_1 = 0;
+          $_2 = 0;
+          break;
+        }
+      }
+    break; // break case 2
+
+
+    case 3:
+    while ($senha != $bruteforce) {
+      $bruteforce = (string) "$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
+      echo "$bruteforce";
+      echo "<br />";
+      $_1++;
+      $tentativas_feitas++;
+      if ($_1 == $qntd) {
+        $_1 = 0;
+        $_2++;
+      }
+      if ($_2 == $qntd) {
+        $_1 = 0;
+        $_2 = 0;
+        $_3++;
+      }
+      if ($_3 == $qntd) {
+        $_1 = 0;
+        $_2 = 0;
+        $_3 = 0;
+        break;
+      }
+    }
+    break; // fim do case 3
+
+
+    case 4:
+    while ($senha != $bruteforce) {
+      $bruteforce = (string) "$quarto[$_4]$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
+      echo "$bruteforce";
+      echo "<br />";
+      $tentativas_feitas++;
+      $_1++;
+      if ($_1 == $qntd) {
+        $_1 = 0;
+        $_2++;
+      }
+      if ($_2 == $qntd) {
+        $_1 = 0;
+        $_2 = 0;
+        $_3++;
+      }
+      if ($_3 == $qntd) {
+        $_1= 0;
+        $_2 = 0;
+        $_3= 0;
+        $_4++;
+      }
+      if ($_4 == $qntd) {
+        $_1 = 0;
+        $_2 = 0;
+        $_3 = 0;
+        $_4 = 0;
+        break;
+      }
+    }
+    break;  // break case 4
+
+    case 5:
+    while ($senha != $bruteforce) {
+      $bruteforce = (string) "$quinto[$_5]$quarto[$_4]$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
+      echo "$bruteforce";
+      echo "<br />";
+      $_1++;
+      $tentativas_feitas++;
+      if ($_1 == $qntd) {
+        $_1 = 0;
+        $_2++;
+      }
+      if ($_2 == $qntd) {
+        $_1 = 0;
+        $_2 = 0;
+        $_3++;
+      }
+      if ($_3 == $qntd) {
+        $_1= 0;
+        $_2 = 0;
+        $_3= 0;
+        $_4++;
+      }
+        if ($_4 == $qntd) {
+          $_1 = 0;
+          $_2 = 0;
+          $_3 = 0;
+          $_4 = 0;
+          $_5++;
+        }
+        if ($_5 == $qntd) {
+          break;
+        }
+      }
+      break; // break case 5
+  } // fim do switch
+} // fim do if modo_turbo
+else {
+
+
+  while ($senha != $bruteforce) {
+    $bruteforce = (string) "$primeiro[$_1]";
+    echo "$bruteforce";
+    echo "<br />";
+    $tentativas_feitas++;
+    $_1++;
+    if ($_1 == $qntd) {
+      $_1 = 0;
+      break;
+    }
   }
+
+
+  // dois caracteres/*
+  while ($senha != $bruteforce) {
+    $bruteforce = "$segundo[$_2]$primeiro[$_1]";
+    echo "$bruteforce";
+    echo "<br />";
+    $_1++;
+    $tentativas_feitas++;
+    if ($_1 == $qntd) {
+      $_1 = 0;
+      $_2++;
+    }
+    if ($_2 == $qntd) {
+      $_1 = 0;
+      $_2 = 0;
+      break;
+    }
+  }
+
+
+  //três caracteres
+  while ($senha != $bruteforce) {
+    $bruteforce = (string) "$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
+    echo "$bruteforce";
+    echo "<br />";
+    $_1++;
+    $tentativas_feitas++;
+    if ($_1 == $qntd) {
+      $_1 = 0;
+      $_2++;
+    }
+    if ($_2 == $qntd) {
+      $_1 = 0;
+      $_2 = 0;
+      $_3++;
+    }
+    if ($_3 == $qntd) {
+      $_1 = 0;
+      $_2 = 0;
+      $_3 = 0;
+      break;
+    }
+  }
+
+
+  //quatro caracteres
+  while ($senha != $bruteforce) {
+    $bruteforce = (string) "$quarto[$_4]$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
+    echo "$bruteforce";
+    echo "<br />";
+    $tentativas_feitas++;
+    $_1++;
+    if ($_1 == $qntd) {
+      $_1 = 0;
+      $_2++;
+    }
+    if ($_2 == $qntd) {
+      $_1 = 0;
+      $_2 = 0;
+      $_3++;
+    }
+    if ($_3 == $qntd) {
+      $_1= 0;
+      $_2 = 0;
+      $_3= 0;
+      $_4++;
+    }
+    if ($_4 == $qntd) {
+      $_1 = 0;
+      $_2 = 0;
+      $_3 = 0;
+      $_4 = 0;
+      break;
+    }
+  }
+
+
+
+  //cinco caracteres
+  while ($senha != $bruteforce) {
+    $bruteforce = (string) "$quinto[$_5]$quarto[$_4]$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
+    echo "$bruteforce";
+    echo "<br />";
+    $_1++;
+    $tentativas_feitas++;
+    if ($_1 == $qntd) {
+      $_1 = 0;
+      $_2++;
+    }
+    if ($_2 == $qntd) {
+      $_1 = 0;
+      $_2 = 0;
+      $_3++;
+    }
+    if ($_3 == $qntd) {
+      $_1= 0;
+      $_2 = 0;
+      $_3= 0;
+      $_4++;
+    }
+      if ($_4 == $qntd) {
+        $_1 = 0;
+        $_2 = 0;
+        $_3 = 0;
+        $_4 = 0;
+        $_5++;
+      }
+      if ($_5 == $qntd) {
+        break;
+      }
+    }
+} // fim do else
+
+} else { //TESTAR SE TEM ALGUMA CHECKBOX OFF
+  header("Location:index.php");
 }
 
 
-// dois caracteres/*
-while ($senha != $bruteforce) {
-  $bruteforce = "$segundo[$_2]$primeiro[$_1]";
-  echo "$bruteforce";
-  echo "<br />";
-  $_1++;
-  $tentativas_feitas++;
-  if ($_1 == $qntd) {
-    $_1 = 0;
-    $_2++;
-  }
-  if ($_2 == $qntd) {
-    $_1 = 0;
-    $_2 = 0;
-    break;
-  }
-}
-
-
-//três caracteres
-while ($senha != $bruteforce) {
-  $bruteforce = (string) "$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
-  echo "$bruteforce";
-  echo "<br />";
-  $_1++;
-  $tentativas_feitas++;
-  if ($_1 == $qntd) {
-    $_1 = 0;
-    $_2++;
-  }
-  if ($_2 == $qntd) {
-    $_1 = 0;
-    $_2 = 0;
-    $_3++;
-  }
-  if ($_3 == $qntd) {
-    $_1= 0;
-    $_2 = 0;
-    $_3= 0;
-    break;
-  }
-}
-
-
-//quatro caracteres
-while ($senha != $bruteforce) {
-  $bruteforce = (string) "$quarto[$_4]$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
-  echo "$bruteforce";
-  echo "<br />";
-  $tentativas_feitas++;
-  $_1++;
-  if ($_1 == $qntd) {
-    $_1 = 0;
-    $_2++;
-  }
-  if ($_2 == $qntd) {
-    $_1 = 0;
-    $_2 = 0;
-    $_3++;
-  }
-  if ($_3 == $qntd) {
-    $_1= 0;
-    $_2 = 0;
-    $_3= 0;
-    $_4++;
-  }
-  if ($_4 == $qntd) {
-    $_1 = 0;
-    $_2 = 0;
-    $_3 = 0;
-    $_4 = 0;
-    break;
-  }
-}
 
 
 
-//cinco caracteres
-while ($senha != $bruteforce) {
-  $bruteforce = (string) "$quinto[$_5]$quarto[$_4]$terceiro[$_3]$segundo[$_2]$primeiro[$_1]";
-  echo "$bruteforce";
-  echo "<br />";
-  $_1++;
-  $tentativas_feitas++;
-  if ($_1 == $qntd) {
-    $_1 = 0;
-    $_2++;
-  }
-  if ($_2 == $qntd) {
-    $_1 = 0;
-    $_2 = 0;
-    $_3++;
-  }
-  if ($_3 == $qntd) {
-    $_1= 0;
-    $_2 = 0;
-    $_3= 0;
-    $_4++;
-  }
-  if ($_4 == $qntd) {
-    $_1 = 0;
-    $_2 = 0;
-    $_3 = 0;
-    $_4 = 0;
-    $_5++;
-  }
-  if ($_5 == $qntd) {
-    break;
-  }
-}
-
-
-echo "<br><b>Você inseriu:<b> " .$senha;
-echo "<br><b>Sua senha é:<b> " .$bruteforce;
-echo "<br /> Foram efetuados " .$tentativas_feitas; echo " tentativas.";
-
-
-echo "</div>";
 
 ?>
+
+  </div>
+
+  <script>
+    pausar();
+  </script>
+
+<div class='quadro2' style='width:450px; height: 300px; float: right; margin-top: -20px;'>
+<?php
+
+if ($senha == $bruteforce) {
+  echo "<br><br><center>Gotcha! Senha encontrada.</center>";
+} else {
+  echo "<br><br><center>Algo deu errado. Desculpe-me o incoveniente.</center>";
+}
+  echo "<br>Você inseriu: <b> " .$senha; echo "</b>";
+  echo "<br>Sua senha é: <b>"; if ($senha == $bruteforce) { echo "$bruteforce </b>"; } else { echo "???</b>"; }
+  //.$bruteforce; echo "</b>";
+  echo "<br /> Foram efetuados <b> " .$tentativas_feitas; echo " </b>tentativas.";
+
+
+  unset($senha, $bruteforce, $tentativas_feitas, $_1);
+
+?>
+
+
+</div>
+</div>
